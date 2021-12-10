0bbc66d652fba49a586cfc4dc7cd4d716f691c34
mira <mira@zhaw.ch>
2021-12-10 14:48:52 +0100

    create stdlib

---
+++ alpine3-pedantic/src/stdLib.h
@@ -0,0 +1,13 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef enum {
+    E_ERROR = -1,
+    E_OK = 0,
+    E_NOT_OK = 1
+} returnType_en;
