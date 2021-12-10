e397a8837ce2ab61d80fdb7e633297e19852406e
mira <mira@zhaw.ch>
2021-12-10 14:48:41 +0100

    create voters base functions

---
+++ alpine3-pedantic/src/voters.c
@@ -0,0 +1,14 @@
+#include "voters.h"
+
+#include "stdLib.h"
+
+int8_t runVoter_A() {
+    return 0;
+}
+int8_t runVoter_B() {
+    return 0;
+}
+
+int8_t runStage2Voter() {
+    return 0;
+}
---
+++ alpine3-pedantic/src/voters.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+int8_t runVoter_A();
+int8_t runVoter_B();
+int8_t runStage2Voter();
