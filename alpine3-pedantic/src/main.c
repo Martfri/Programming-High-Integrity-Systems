f2c7bc9e4bbd07cf657dfc475d461ca553c74dec
mira <mira@zhaw.ch>
2021-12-10 14:49:33 +0100

    create base main function

--- alpine3-pedantic/src/main.c
+++ alpine3-pedantic/src/main.c
@@ -1,56 +1,42 @@
-#include <pthread.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-void *readCLI(void *_exitCmd) {
-    bool *exitCmd = (bool *)_exitCmd;
-    char line[50];
-    int length = 0;
-
-    printf("CLI thread started\n");
-
-    while (false == *exitCmd) {
-        length = 0;
-        while ((line[length] = getchar()) != '\n') {
-            length++;
-            if (length >= 49)
-                break;
-        }
-        line[length] = '\0';
-
-        if ((line[0] == 'q') && (strlen(line) == 1)) {
-            *exitCmd = true;
-            printf("Stopped by user\n");
-        }
-    }
-    printf("CLI thread terminating\n");
-    return NULL;
-}
+#include "cli.h"
+#include "distFunc.h"
+#include "sensors.h"
+#include "stdLib.h"
+#include "voters.h"
 
 int main() {
+    // returnType_en retVal;
     pthread_t cliThread;
-    bool exitCmd = false;
+    bool rcvdExitCmd = false;
+    int32_t sensorReadings[NR_OF_SENSORS];
 
     printf("Starting Program\n");
 
     /* Start keyboard listener thread */
-    if (0 != pthread_create(&cliThread, (pthread_attr_t *)NULL, readCLI, (void *)&exitCmd)) {
+    if (0 != pthread_create(&cliThread, (pthread_attr_t *)NULL, readCLI, (void *)&rcvdExitCmd)) {
         printf("Error: Failed to create CLI thread, terminating program\n");
         exit(EXIT_FAILURE);
     }
 
-    while (false == exitCmd) {
+    /* Main Loop */
+    while (false == rcvdExitCmd) {
+        readSensors(sensorReadings);
+
+        computeDistance_BlockA();
+
+        computeDistance_BlockB();
+
+        runStage2Voter();
+
+        /* Display System Decision */
         sleep(1);
     }
 
+    /* Wait For CLI thread to terminate */
     if (0 != pthread_join(cliThread, NULL)) {
         printf("Error: Failed to wait for CLI thread to terminate\n");
     }
 
-    printf("Terminating Program\n");
+    printf("Terminating program\n");
     return 0;
 }
