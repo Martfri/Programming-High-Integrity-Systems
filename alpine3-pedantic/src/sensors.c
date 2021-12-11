c08685551e4d2f20fadfeb071e60491e0bc8f9a3
mira <mira@zhaw.ch>
2021-12-11 09:13:03 +0100

    add function descriptions

--- alpine3-pedantic/src/sensors.c
+++ alpine3-pedantic/src/sensors.c
@@ -2,6 +2,14 @@
 
 #include "stdLib.h"
 
+/**
+ * @brief Returns the output value from a simulated sensor.
+ * Selects the right file to read according to the sensor index
+ * and reads and returns the written value * 10 to avoid floating point values
+ *
+ * @param sensorIdx: sensor index
+ * @return sensor reading in (10*m)
+ */
 int32_t getSensorReading(uint8_t sensorIdx) {
     switch (sensorIdx) {
         case 0:
@@ -31,18 +39,32 @@ int32_t getSensorReading(uint8_t sensorIdx) {
     return reading;
 }
 
+/**
+ * @brief Prints to the console the current readings of all the sensors
+ *
+ * @param sensorReadings: Array of sensor readings
+ */
 void printSensorReadings(sensor_t sensorReadings[]) {
     for (uint8_t sensorIdx = 0; sensorIdx < NR_OF_SENSORS; sensorIdx++) {
         printf("Sensor %d: %d\n", sensorIdx, sensorReadings[sensorIdx].reading);
     }
 }
 
+/**
+ * @brief Loops through all the sensor and fills the array of readings with 
+ * the most updated sensor readings
+ *
+ * @param sensorReadings: Array of sensor readings
+ * @return E_OK = 0
+ */
 returnType_en readSensors(sensor_t sensorReadings[]) {
     for (uint8_t sensorIdx = 0; sensorIdx < NR_OF_SENSORS; sensorIdx++) {
         sensorReadings[sensorIdx].reading = getSensorReading(sensorIdx);
     }
 
+#ifdef DEBUG
     printSensorReadings(sensorReadings);
+#endif
 
     return E_OK;
 }
--- alpine3-pedantic/src/sensors.h
+++ alpine3-pedantic/src/sensors.h
@@ -1,20 +1,48 @@
+/**
+ * @file sensors.h
+ * @brief Implementation of the platform specific command line interface.
+ *
+ * @note the gPTP protocol requires the following functions:
+ */
 #ifndef SENSORS_H
 #define SENSORS_H
 
 #include <stdint.h>
 
 #include "stdLib.h"
+
 #define NR_OF_SENSORS 3
 
+/**
+ * @brief Loops through all the sensor and fills the array of readings with 
+ * the most updated sensor readings
+ *
+ * @param sensorReadings: Array of sensor readings
+ * @return E_OK = 0
+ */
 typedef enum {
     SENSOR_OK = 0,
     SENSOR_NOK
 } sensorState_en;
 
+/**
+ * @brief Loops through all the sensor and fills the array of readings with 
+ * the most updated sensor readings
+ *
+ * @param sensorReadings: Array of sensor readings
+ * @return E_OK = 0
+ */
 typedef struct sensor_t {
     sensorState_en state;
     uint32_t reading;
 } sensor_t;
 
+/**
+ * @brief Loops through all the sensor and fills the array of readings with 
+ * the most updated sensor readings
+ *
+ * @param sensorReadings: Array of sensor readings
+ * @return E_OK = 0
+ */
 returnType_en readSensors(sensor_t sensorReadings[]);
 #endif
