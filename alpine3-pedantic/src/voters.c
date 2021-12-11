663e397b8e35981c3c6537a1439a3815c5f4dfae
mira <mira@zhaw.ch>
2021-12-11 09:36:11 +0100

    remove sensor reading check

--- alpine3-pedantic/src/voters.c
+++ alpine3-pedantic/src/voters.c
@@ -12,13 +12,6 @@
 returnType_en runVoter_A(sensor_t sensorReadings[], int32_t *bestValue) {
     returnType_en retVal = E_NOT_OK;
 
-    for (uint8_t sensorIdx = 0; sensorIdx < NR_OF_SENSORS; sensorIdx++) {
-        int32_t reading = sensorReadings[sensorIdx].reading;
-        if (20 >= reading || 200 < reading) {
-            /* error handling for sensor error*/
-        }
-    }
-
     int32_t reading_s1 = sensorReadings[0].reading;
     int32_t reading_s2 = sensorReadings[1].reading;
     int32_t reading_s3 = sensorReadings[2].reading;
