41edbba89e07675b53b741b525ad9498becbaae0
mira <mira@zhaw.ch>
2021-12-11 09:59:55 +0100

    improve voterA + create allSensorsOperational()

--- alpine3-pedantic/src/voters.c
+++ alpine3-pedantic/src/voters.c
@@ -3,6 +3,29 @@
 #include "stdLib.h"
 
 /**
+ * @brief checks if all sensors are operational
+ *
+ * @param sensorReadings: Array of sensor readings
+ * @return bool true if all the sensors are operational, else false
+ */
+bool allSensorsOperational(sensor_t sensorReadings[]) {
+    bool allOperational = false;
+    uint8_t nrOfOperationalSensors = 0;
+
+    for (uint8_t sensorIdx = 0; sensorIdx < NR_OF_SENSORS; sensorIdx++) {
+        if (SENSOR_OK == sensorReadings[sensorIdx].state) {
+            nrOfOperationalSensors++;
+        }
+    }
+
+    if (NR_OF_SENSORS == nrOfOperationalSensors) {
+        allOperational = true;
+    }
+
+    return allOperational;
+}
+
+/**
  * @brief runs the implementation A of first stage voter
  *
  * @param sensorReadings: Array of sensor readings
@@ -12,23 +35,32 @@
 returnType_en runVoter_A(sensor_t sensorReadings[], int32_t *bestValue) {
     returnType_en retVal = E_NOT_OK;
 
-    int32_t reading_s1 = sensorReadings[0].reading;
-    int32_t reading_s2 = sensorReadings[1].reading;
-    int32_t reading_s3 = sensorReadings[2].reading;
+    if (true == allSensorsOperational(sensorReadings)) {
+        int32_t reading_s1 = sensorReadings[0].reading;
+        int32_t reading_s2 = sensorReadings[1].reading;
+        int32_t reading_s3 = sensorReadings[2].reading;
 
-    int32_t diff1 = reading_s1 - reading_s2;
-    int32_t diff2 = reading_s2 - reading_s3;
+        int32_t diff1 = reading_s1 - reading_s2;
+        int32_t diff2 = reading_s2 - reading_s3;
 
-    if (2 > diff1 || 2 > diff2) {
-        if (diff1 < diff2) {
-            *bestValue = (reading_s1 + reading_s2) / (NR_OF_SENSORS - 1);
-        } else if (diff2 < diff1) {
-            *bestValue = (reading_s2 + reading_s3) / (NR_OF_SENSORS - 1);
+        if (2 > diff1 || 2 > diff2) {
+            if (diff1 < diff2) {
+                /*  S1--S2-----S3 */
+                *bestValue = (reading_s1 + reading_s2) / (NR_OF_SENSORS - 1);
+            } else if (diff2 < diff1) {
+                /*  S1-----S2--S3 */
+                *bestValue = (reading_s2 + reading_s3) / (NR_OF_SENSORS - 1);
+            } else {
+                /*  S1--S2--S3 */
+                *bestValue = (reading_s1 + reading_s2 + reading_s3) / NR_OF_SENSORS;
+            }
+            retVal = E_OK;
         } else {
-            *bestValue = (reading_s1 + reading_s2 + reading_s3) / NR_OF_SENSORS;
+            /* If any diff is more than 2 means one of the sensors is faulty*/
+            retVal = E_NOT_OK;
         }
-        retVal = E_OK;
     } else {
+        printf("Not all the sensors are operational.\n");
         retVal = E_NOT_OK;
     }
 
