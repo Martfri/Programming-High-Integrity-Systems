65262d1e08e0b563b03dcdb15dc5016a98bbea31
mira <mira@zhaw.ch>
2021-12-16 11:28:58 +0100

    Change VoterA logic + implement Stage2 Voter

--- alpine3-pedantic/src/voters.c
+++ alpine3-pedantic/src/voters.c
@@ -32,33 +32,18 @@ bool allSensorsOperational(sensor_t sensorReadings[]) {
  * @param bestValue: pointer to a variable to hold best value from all the readings
  * @return returnType_en E_OK in case the best value was computed sucessfuly, else E_NOT_OK
  */
-returnType_en runVoter_A(sensor_t sensorReadings[], int32_t* bestValue) {
+returnType_en runVoter_A(sensor_t sensorReadings[], uint8_t* bestValue) {
     returnType_en retVal = E_NOT_OK;
 
-    if (true == allSensorsOperational(sensorReadings)) {
-        uint32_t diff1 = abs(sensorReadings[0].reading - sensorReadings[1].reading);
-        uint32_t diff2 = abs(sensorReadings[1].reading - sensorReadings[2].reading);
+    // for (int i = 0; i < 3; i++) printf("value %d: %d\n", i, sensorReadings[i].reading);
 
-        if (SENSOR_ACCURACY >= diff1 || SENSOR_ACCURACY >= diff2) {
-            if (diff1 < diff2) {
-#ifdef DEBUG
-                /*  S1--S2-----S3 */
-                printf("Case 1\n");
-#endif
-                *bestValue = (sensorReadings[0].reading + sensorReadings[1].reading) / (NR_OF_SENSORS - 1);
-            } else if (diff2 < diff1) {
-#ifdef DEBUG
-                /*  S1-----S2--S3 */
-                printf("Case 2\n");
-#endif
-                *bestValue = (sensorReadings[1].reading + sensorReadings[2].reading) / (NR_OF_SENSORS - 1);
-            } else {
-#ifdef DEBUG
-                /*  S1--S2--S3 */
-                printf("Case 3\n");
-#endif
-                *bestValue = (sensorReadings[0].reading + sensorReadings[1].reading + sensorReadings[2].reading) / NR_OF_SENSORS;
-            }
+    if (true == allSensorsOperational(sensorReadings)) {
+        uint8_t diff1 = abs(sensorReadings[0].reading - sensorReadings[1].reading);
+        uint8_t diff2 = abs(sensorReadings[1].reading - sensorReadings[2].reading);
+        uint8_t diff3 = abs(sensorReadings[0].reading - sensorReadings[2].reading);
+
+        if (SENSOR_ACCURACY >= diff1 && SENSOR_ACCURACY >= diff2 && SENSOR_ACCURACY >= diff3) {
+            *bestValue = (sensorReadings[0].reading + sensorReadings[1].reading + sensorReadings[2].reading) / NR_OF_SENSORS;
             retVal = E_OK;
         } else {
             printf("Sensor threshold is greater than allowed.\n");
@@ -90,6 +75,6 @@ returnType_en runVoter_B(sensor_t sensorReadings[]) {
  * @return
  */
 returnType_en runStage2Voter(bool distanceIsSafe_A, bool distanceIsSafe_B, bool* enterSafeState) {
-    *enterSafeState = !distanceIsSafe_A;
+    *enterSafeState = !distanceIsSafe_A | !distanceIsSafe_B;
     return E_OK;
 }
