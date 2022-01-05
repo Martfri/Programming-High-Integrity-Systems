fc9a7ac5d204522adea69d7cf1ed48733d934bfc
Martin Frick <73395595+Martfri@users.noreply.github.com>
2022-01-05 15:05:46 +0100

    Create Integration.py
    added Integration of Voters

---
+++ alpine3-pedantic/src/test/Integration.py
@@ -0,0 +1,29 @@
+import Voter1
+import Voter2
+import Voter3
+
+
+def Integration(s1, s2, s3):
+    ConsideredValue = 0 
+    SensorValues = [s1, s2, s3]
+    print(SensorValues)
+    Safestate = True
+    Operational = False
+    FlowControl = 0 
+
+    OutcomeVoter1 = Voter1.voter1(SensorValues)
+    OutcomeVoter2 = Voter2.voter2(SensorValues)
+
+    OutcomeVoter3 = Voter3.voter3(OutcomeVoter1, OutcomeVoter2)
+
+    if OutcomeVoter3 == True:
+        Safestate = True
+        Operational = False
+    else:
+        Safestate = False
+        Operational = True
+
+    print("Roboter is in Safestate: %s" %"True" if Safestate else "Roboter is in Safestate: False")
+    print("h")
+
+    return 0
