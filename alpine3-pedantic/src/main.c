300ac8e2bbe9c5f51cff4304476ef8ad9b9f440f
raphael.moehr@ost.ch <raphael.moehr@ost.ch>
2022-01-06 13:56:14 +0100

    changed position of start val

--- alpine3-pedantic/src/main.c
+++ alpine3-pedantic/src/main.c
@@ -112,9 +112,9 @@ int main() {
       cast to some type, this message is not given.
     */
     while (false == rcvdExitCmd) {  //lint !e731
+        start = clock();
         enterSafeState = true;
         flowControl = 0;
-        start = clock();
         retVal = readSensors(socket_desc, sensorReadings);
 
         /* It is advised not to perform bitwise operations on enums because it is not possible to define if an enum is a signed or unsigned integer.
