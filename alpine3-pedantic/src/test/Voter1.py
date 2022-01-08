cf3208ad170b49803e16cc47eebd2100a5b32d36
raphael.moehr@ost.ch <raphael.moehr@ost.ch>
2022-01-08 21:42:44 +0100

    voter logic changed

--- alpine3-pedantic/src/test/Voter1.py
+++ alpine3-pedantic/src/test/Voter1.py
@@ -1,13 +1,13 @@
 
 
 def voter1(SensorValues):
-    if isOperational(SensorValues) == True:
+    if isSafeRange(SensorValues) == True:
         diff1 = abs(SensorValues[0] - SensorValues[1])
         diff2 = abs(SensorValues[1] - SensorValues[2])
         diff3 = abs(SensorValues[0] - SensorValues[2])
 
-        if 0.2 >= diff1 and 0.2 >= diff2 and 0.2 >= diff3:
-            Value = (SensorValues[0] + SensorValues[1] + SensorValues[2])/3
+        if 2 < diff1 or 2 < diff2 or 2 < diff3:
+            #Value = (SensorValues[0] + SensorValues[1] + SensorValues[2])/3
             #print("Voter1: Sensor values match and are operational")
             return True
 
@@ -18,16 +18,14 @@ def voter1(SensorValues):
 
     else:
         #print("Voter1: Sensors are not operational")
-        return False
+        return True
 
 
 
-def isOperational(sensorvalues):
+def isSafeRange(sensorvalues):
     for i in range(3):
-        if sensorvalues[i] == 0 or sensorvalues[i] == 2:
+        if 40 <= sensorvalues[i] <= 184:
             #print("Voter1: at least one faulty sensor detected")
-            return False
-        elif 4 <= sensorvalues[i] <= 18.4:
             continue
         else:
             #print("Voter1: at least one sensor detected object in unsafe distance")
