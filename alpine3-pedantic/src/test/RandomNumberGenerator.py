61d603720701f364d106924debd81c66d9961554
raphael.moehr@ost.ch <raphael.moehr@ost.ch>
2022-01-08 21:44:22 +0100

    random gen changed from uniform to gaussian

--- alpine3-pedantic/src/test/RandomNumberGenerator.py
+++ alpine3-pedantic/src/test/RandomNumberGenerator.py
@@ -2,7 +2,7 @@ import decimal
 import random
 
 def RandNum():
-   num1 = int(decimal.Decimal(random.randrange(400, 2000)/10 if(random.choice([1,2]) == 1) else random.choice([0,20])))
-   num2 = int(decimal.Decimal(random.randrange(400, 2000)/10 if(random.choice([1,2]) == 1) else random.choice([0,20])))
-   num3 = int(decimal.Decimal(random.randrange(400, 2000)/10 if(random.choice([1,2]) == 1) else random.choice([0,20])))
+   num1 = int(decimal.Decimal(random.gauss(120, 60))) % 255
+   num2 = int(decimal.Decimal(random.gauss(120, 60))) % 255
+   num3 = int(decimal.Decimal(random.gauss(120, 60))) % 255
    return [num1, num2, num3]
